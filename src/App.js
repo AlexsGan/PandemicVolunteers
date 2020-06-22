dbc202f5822abb7f1b2909783781113ed6b6c649
Felix Liu <felixliu.liu@mail.utoronto.ca>
2020-06-22 02:33:39 -0400

    Added file structure & starter files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+
+// Import react router
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Import components
+import Landing from "./components/Landing";
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' render={() => 
+                            (<Landing/>)}/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );  
+  }
 }
 
 export default App;
