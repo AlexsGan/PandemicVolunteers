dc135314944066f981ba4b2e8584e77bc9287112
alexgan <alex.gan@mail.utoronto.ca>
2020-06-30 13:25:28 -0400

    deleted GroupChatInput, modified GroupChat

--- src/components/GroupChat/index.js
+++ src/components/GroupChat/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import GroupChatEnter from "./GroupChatInput";
 
 import TextField from '@material-ui/core/TextField';
 import Button from "@material-ui/core/Button";
@@ -11,25 +10,45 @@ import "./styles.css";
 
 /* Component for group chat */
 class GroupChat extends React.Component {
+
   state = {
-    message: "",
-    messageError: false,
-    submitMessage: false,
-    redirect: false
+    date : '', 
   };
 
-  handleChange = event => {
-    const target = event.target;
-    this.setState({
-        [target.name]: target.value
-    });
-    console.log("handleChange called");
-  }
+  // componentDidMount() {
+  //   var that = this;
+  //   var date = new Date().getDate(); //Current Date
+  //   var month = new Date().getMonth() + 1; //Current Month
+  //   var year = new Date().getFullYear(); //Current Year
+  //   var hours = new Date().getHours(); //Current Hours
+  //   var min = new Date().getMinutes(); //Current Minutes
+  //   var sec = new Date().getSeconds(); //Current Seconds
+  //   that.setState({
+  //     //Setting the value of the date time
+  //     date:
+  //       date + '/' + month + '/' + year + ' ' + hours + ':' + min + ':' + sec,
+  //   });
+  // }
 
-  handleSubmit = event => {
-    // BACKEND: Send user message to server and add message in GroupChatInput
-    console.log(this.state.message); // added by Alex
-    setTimeout(() => {this.setState({submitMessage: true})}, 500);
+  setCurrentToDoItem = toDoItem => {
+    this.setState({
+      currentToDoItem: toDoItem
+    });
+  };
+
+  saveToDoListItem = toDoItem => {
+    this.setState({
+      toDoList: [toDoItem, ...this.state.toDoList],
+    });
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentToDoItem: null,
+      toDoList: []
+    };
   }
 
   render() {
@@ -41,13 +60,40 @@ class GroupChat extends React.Component {
       return <Redirect to="/GroupChat/GroupChatInput"/>;
     }
     return (
-        <Container className="message-entry" maxWidth="sm">
-          <GroupChatEnter 
-            message = {this.state.message}
-            handleChange={this.handleChange}
-            handleSubmit={this.handleSubmit}
-            messageError={this.state.messageError}
-          />
+        <Container>
+          <Container className="message-entry" maxWidth="sm">
+            <TextField 
+                name="message"
+                variant="outlined"
+                label = "Message to group:"
+                onChange={event => this.setCurrentToDoItem(event.target.value)} 
+                fullWidth
+            />
+            <Button 
+              variant="contained" 
+              onClick={() => this.saveToDoListItem(this.state.currentToDoItem)}
+              fullWidth 
+            >
+              Post
+            </Button>
+
+            {this.state.toDoList.map((item, index) => (
+              <Box>
+              <Box 
+                className="posted-chats" 
+                component="span" 
+                display="flex"
+                bgcolor="grey.300"
+                p={1} 
+              > 
+                {item}{""}
+              </Box>
+              {/* <Box className="message-time">
+                {this.state.date}
+              </Box> */}
+              </Box>
+            ))}
+          </Container>
         </Container>
     );
   }
