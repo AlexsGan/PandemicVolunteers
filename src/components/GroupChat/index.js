e05eaf40e6ac564c9e820addac8dc53ac5a51494
alexgan <alex.gan@mail.utoronto.ca>
2020-06-30 16:43:50 -0400

    modified GroupChat

--- src/components/GroupChat/index.js
+++ src/components/GroupChat/index.js
@@ -79,7 +79,7 @@ class GroupChat extends React.Component {
   handleValidation(){
     if (this.state.newMessage != '' && this.state.newMessage.length < 500) // post must have 1 < Character < 500
       return true
-    
+    alert("Invalid message length!")
     return false
   }
 
@@ -111,7 +111,7 @@ class GroupChat extends React.Component {
       return <Redirect to="/GroupChat/GroupChatInput"/>;
     }
     return (
-        <Container>
+        <Container className="group-chat-box" maxWidth="md">
           <Container className="message-entry" maxWidth="sm">
             <TextField 
                 name="message"
@@ -121,6 +121,7 @@ class GroupChat extends React.Component {
                 onChange={this.handleChangeTextbox}
                 fullWidth
             />
+            
             <Button 
               variant="contained" 
               onClick={this.onSubmit}
@@ -128,6 +129,7 @@ class GroupChat extends React.Component {
             >
               Post
             </Button>
+
             <Button 
               variant="contained" 
               onClick={this.onCancel}
@@ -135,13 +137,15 @@ class GroupChat extends React.Component {
             >
               Cancel
             </Button>
-            {this.state.messageList.map((item, index) => (
+            
+            {this.state.messageList.map((item, index) => ( // output message
               <Box>
               <Box 
                 className="posted-chats" 
                 component="span" 
                 display="flex"
                 bgcolor="grey.300"
+                borderRadius={10}
                 p={1} 
               > 
                 {item}{""}
@@ -151,6 +155,23 @@ class GroupChat extends React.Component {
               </Box> */}
               </Box>
             ))}
+
+
+              {/* the Box below is hardcoded: will be removed when back-end is implemented */}
+              <Box 
+                className="posted-chats" 
+                component="span" 
+                display="flex"
+                bgcolor="grey.300"
+                borderRadius={10}
+                p={1} 
+              > 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+                sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Box>
           </Container>
         </Container>
     );
