ea3900cf85c89afc632dda4a9be6abecde724776
alexgan <alex.gan@mail.utoronto.ca>
2020-07-02 11:57:42 -0400

    added dashboard copied from groupchat, modified appearance

--- src/components/GroupChat/index.js
+++ src/components/GroupChat/index.js
@@ -2,15 +2,15 @@ import React from "react";
 
 import TextField from '@material-ui/core/TextField';
 import Button from "@material-ui/core/Button";
-import { Container, Box, GridList } from "@material-ui/core";
+import { Container, Box, Grid, GridList } from "@material-ui/core";
 import { Redirect } from "react-router";
 
 import "./styles.css";
-
+import Navbar from "../LandingView/Navbar";
 
 /* Component for group chat */
 class GroupChat extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       newMessage: '',
@@ -22,85 +22,38 @@ class GroupChat extends React.Component {
     this.onSubmit = this.onSubmit.bind(this)
     this.onCancel = this.onCancel.bind(this)
   }
-  // state = {
-  //   date : '', 
-  //   messageValue: '',
-  // };
 
-  // // componentDidMount() {
-  // //   var that = this;
-  // //   var date = new Date().getDate(); //Current Date
-  // //   var month = new Date().getMonth() + 1; //Current Month
-  // //   var year = new Date().getFullYear(); //Current Year
-  // //   var hours = new Date().getHours(); //Current Hours
-  // //   var min = new Date().getMinutes(); //Current Minutes
-  // //   var sec = new Date().getSeconds(); //Current Seconds
-  // //   that.setState({
-  // //     //Setting the value of the date time
-  // //     date:
-  // //       date + '/' + month + '/' + year + ' ' + hours + ':' + min + ':' + sec,
-  // //   });
-  // // }
-
-  // submitChar = newMessage => {
-  //   this.setState({
-  //     submitChar: newMessage
-  //   });
-  // };
-
-  // submitMessage = newMessage => {
-  //   this.setState({
-  //     messageList: [newMessage, ...this.state.messageList],
-  //   });
-  //   this.clearData()
-  // };
-
-  // clearData(){
-  //   this.setState({
-  //     value: ''
-  //   })
-  // }
-
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     submitChar: null,
-  //     messageList: []
-  //   };
-  // }
-
-  clearData(){
+  clearData() {
     this.setState({
       newMessage: ''
     })
   }
 
-  handleValidation(){
+  handleValidation() {
     if (this.state.newMessage != '' && this.state.newMessage.length < 500) // post must have 1 < Character < 500
       return true
     alert("Invalid message length!")
     return false
   }
 
-  handleChangeTextbox(e){
+  handleChangeTextbox(e) {
     this.setState({
       newMessage: e.target.value
     })
   }
 
-  onSubmit(e){
+  onSubmit(e) {
     e.preventDefault();
-    if(this.handleValidation()){
+    if (this.handleValidation()) {
       //alert("Form submit")
       this.setState({
-             messageList: [this.state.newMessage, ...this.state.messageList],
-           });
+        messageList: [this.state.newMessage, ...this.state.messageList],
+      });
       this.clearData()
     }
   }
 
-  onCancel(e){
+  onCancel(e) {
     e.preventDefault()
     //alert("Cancel")
     this.clearData()
@@ -108,73 +61,82 @@ class GroupChat extends React.Component {
 
   render() {
     if (this.state.redirect) {
-      return <Redirect to="/GroupChat/GroupChatInput"/>;
+      return <Redirect to="/GroupChat/GroupChatInput" />;
     }
     return (
+      <div>
+        <Navbar />
         <Container className="group-chat-box" maxWidth="md">
           <Container className="message-entry" maxWidth="sm">
-            <TextField 
-                name="message"
-                variant="outlined"
-                label = "Message to group:"
-                value={this.state.newMessage} 
-                onChange={this.handleChangeTextbox}
-                fullWidth
-            />
-
-            <Button 
-              variant="contained" 
-              onClick={this.onSubmit}
-              fullWidth 
-            >
-              Post
-            </Button>
-
-            <Button 
-              variant="contained" 
-              onClick={this.onCancel}
-              fullWidth 
-            >
-              Cancel
-            </Button>
+            <Grid className="messages-grid" container spacing={2}>
+              <Grid item xs="12">
+                <TextField
+                  name="message"
+                  variant="outlined"
+                  label="Message to group:"
+                  value={this.state.newMessage}
+                  onChange={this.handleChangeTextbox}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <Button
+                  variant="contained"
+                  onClick={this.onSubmit}
+                  fullWidth
+                  color="primary"
+                >
+                  Post
+                </Button>
+              </Grid>
+              <Grid item xs={4}>
+                <Button
+                  variant="contained"
+                  onClick={this.onCancel}
+                >
+                  Cancel
+                </Button>
+              </Grid>
+            </Grid>
             <Container className="message-entry" maxWidth="sm">
 
-            {this.state.messageList.map((item, index) => ( // output message
-              <Box>
-              <Box 
-                className="posted-chats" 
-                component="span" 
-                display="flex"
-                bgcolor="grey.300"
-                borderRadius={10}
-                p={1} 
-              > 
-                {item}{""}
-              </Box>
-              {/* <Box className="message-time">
+              {this.state.messageList.map((item, index) => ( // output message
+                <Box>
+                  <Box
+                    className="posted-chats"
+                    component="span"
+                    display="flex"
+                    bgcolor="grey.300"
+                    borderRadius={10}
+                    p={1}
+                  >
+                    {item}{""}
+                  </Box>
+                  {/* <Box className="message-time">
                 {this.state.date}
               </Box> */}
-              </Box>
-            ))}
+                </Box>
+              ))}
               {/* the Box below is hardcoded: will be removed when back-end is implemented */}
-              <Box 
-                className="posted-chats" 
-                component="span" 
+              <Box
+                className="posted-chats"
+                component="span"
                 display="flex"
                 bgcolor="grey.300"
                 borderRadius={10}
-                p={1} 
-              > 
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+                p={1}
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
                 sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </Box>
 
-              </Container>
+            </Container>
           </Container>
         </Container>
+      </div>
     );
   }
 }
--- src/components/Requests/index.js
+++ src/components/Requests/index.js
@@ -1,14 +1,142 @@
 import React from "react";
 
-/* Component for group chat */
+import TextField from '@material-ui/core/TextField';
+import Button from "@material-ui/core/Button";
+import { Container, Box, Grid, GridList } from "@material-ui/core";
+import { Redirect } from "react-router";
+
+import "./styles.css";
+import Navbar from "../LandingView/Navbar";
+
+
+/* Component for Requests */
 class Requests extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newMessage: '',
+      messageList: []
+    }
+
+    this.clearData = this.clearData.bind(this)
+    this.handleChangeTextbox = this.handleChangeTextbox.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+    this.onCancel = this.onCancel.bind(this)
+  }
+
+  clearData() {
+    this.setState({
+      newMessage: ''
+    })
+  }
+
+  handleValidation() {
+    if (this.state.newMessage != '' && this.state.newMessage.length < 500) // post must have 1 < Character < 500
+      return true
+    alert("Invalid message length!")
+    return false
+  }
+
+  handleChangeTextbox(e) {
+    this.setState({
+      newMessage: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    if (this.handleValidation()) {
+      //alert("Form submit")
+      this.setState({
+        messageList: [this.state.newMessage, ...this.state.messageList],
+      });
+      this.clearData()
+    }
+  }
+
+  onCancel(e) {
+    e.preventDefault()
+    //alert("Cancel")
+    this.clearData()
+  }
+
   render() {
+    if (this.state.redirect) {
+      return <Redirect to="/GroupChat/GroupChatInput" />;
+    }
     return (
-        <div>
-            TODO
-        </div>
-      );
-    
+      <div>
+        <Navbar />
+        <Container className="requests-box" maxWidth="lg">
+          <Container className="requests-container" maxWidth="md">
+            <Grid className="requests-grid" container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  name="message"
+                  variant="outlined"
+                  label="Add a request:"
+                  value={this.state.newMessage}
+                  onChange={this.handleChangeTextbox}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <Button
+                  variant="contained"
+                  onClick={this.onSubmit}
+                  color="primary"
+                >
+                  Post
+                </Button>
+              </Grid>
+              <Grid item xs={3}>
+                <Button
+                  variant="contained"
+                  onClick={this.onCancel}
+                >
+                  Cancel
+                </Button>
+              </Grid>
+            </Grid>
+
+
+            {this.state.messageList.map((item, index) => ( // add requests 
+              <Box>
+                <Box
+                  className="posted-chats"
+                  component="span"
+                  display="flex"
+                  bgcolor="grey.300"
+                  borderRadius={10}
+                  p={1}
+                >
+                  {item}{""}
+                </Box>
+                {/* <Box className="message-time">
+                {this.state.date}
+              </Box> */}
+              </Box>
+            ))}
+            {/* the Box below is hardcoded: will be removed when back-end is implemented */}
+            <Box
+              className="posted-chats"
+              component="span"
+              display="flex"
+              bgcolor="grey.300"
+              borderRadius={10}
+              p={1}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+              ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+              sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Box>
+
+          </Container>
+        </Container>
+      </div>
+    );
   }
 }
 
