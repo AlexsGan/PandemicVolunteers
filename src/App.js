e1cb07b9c2aa37a7d084fdd42d1202595e46af30
Felix Liu <felixliu.liu@mail.utoronto.ca>
2020-07-03 19:35:02 -0400

    Redesigned navbar using MaterialUI components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 // Import components
 import GroupChat from "./components/GroupChat";
-import View from "./components/LandingView";
+import LandingView from "./components/LandingView";
 import Requests from "./components/Requests";
 import Register from "./components/Register";
 import ProfileWizard from "./components/Register/ProfileWizard";
@@ -20,19 +20,14 @@ class App extends React.Component {
       <div>
         <BrowserRouter>
           <Switch>
-            {/* <Route exact path='/' render={() => 
-                            (<View/>)}/> */}
             <Route exact path='/group-chat' component={GroupChat} />
             <Route exact path='/requests' component={Requests} />
-            {/* <Route exact path ='/register' render ={() => 
-                            (<Register/>)}/> 
-            <Route exact path ='/register/create-profile' render = {() =>
-                            (<ProfileWizard/>)}/> */}
-            <Route exact path='/' component={View} />
+            <Route exact path='/' component={LandingView} />
             <Route exact path ='/login' component={Login} />
             <Route exact path ='/register' component={Register} />
             <Route exact path ='/register/create-profile' component={ProfileWizard} />
             <Route exact path ='/profile' component={UserProfile} />
+            <Route exact path ='/about' component={LandingView} />
           </Switch>
         </BrowserRouter>
       </div>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,17 @@
 import React from "react";
-import {Grid, Button} from "@material-ui/core"
+import { Typography, Tabs, AppBar, Tab } from "@material-ui/core"
 import { Redirect } from "react-router-dom"
 import "./styles.css";
 
 /* Navigation Bar Component for Landing page */
 class Navbar extends React.Component {
     state = {
-        redirectPath: "",
+        redirectPath: this.props.currentPath,
     }
 
-    handleClick = (event) => {
-        const target = event.currentTarget;
-        this.setState({redirectPath: target.value});
-        console.log(target.value)
+    handleClick = (event, newValue) => {
+        this.setState({redirectPath: newValue});
+        console.log(newValue)
     }
 
     render() {
@@ -21,7 +20,7 @@ class Navbar extends React.Component {
         } = this.props;
 
         // Handle redirects
-        if (this.state.redirectPath !== "") {
+        if (this.state.redirectPath !== currentPath) {
             console.log(this.state.redirectPath)
             return (
                 <Redirect 
@@ -34,74 +33,26 @@ class Navbar extends React.Component {
         }
         
         return (
-            <Grid container spacing={1} className="background">
-                <Grid item xs={4}>
-                <div className="titleBackground">
-                    <h1 className="appName">To be named later</h1>
-                </div>
-                </Grid>
-                <Grid item xs={2}>
-                <Button
-                    value="/requests"
-                    variant="contained"
-                    color="default"
-                    onClick={this.handleClick}
-                    disabled={currentPath === "/requests"}
+            <AppBar position="static">
+                <Tabs
+                    className="navbar__tabs"
+                    value={this.state.redirectPath}
+                    onChange={this.handleClick}
+                    centered
                 >
-                    <h3 className="nav_text">Requests Dashboard</h3>
-                </Button> 
-                </Grid>
-                <Grid item xs={2}>
-                <Button
-                    value="/group-chat"
-                    variant="contained"
-                    color="default"
-                    onClick={this.handleClick}
-                    disabled={currentPath === "/group-chat"}
-                >
-                    <h3 className="nav_text">Group Chat</h3>
-                </Button> 
-                </Grid>
-                <Grid item xs={2}>
-                <Button
-                    value=""
-                    variant="contained"
-                    color="default"
-                    onClick={this.handleClick}
-                    disabled={currentPath === ""}
-                    >
-                    <h3 className="nav_text">About Us</h3>
-                </Button> 
-                </Grid>
-                <Grid item xs={2}>
-                {/* If user object was provided, we are logged in */}
-                {this.props.userObject !== null ? (
-                        // Access profile
-                        <Button
-                            value="/profile"
-                            variant="contained"
-                            color="default"
-                            onClick={this.handleClick}
-                            disabled={currentPath === "/profile"}
-                        >
-                            <h3 className="nav_text">Profile</h3>
-                        </Button> 
-                    ) : (
-                        // Access login
-                        <Button
-                            value="/login"
-                            variant="contained"
-                            color="default"
-                            onClick={this.handleClick}
-                            disabled={currentPath === "/login"}
-                        >
-                            <h3 className="nav_text">Log In</h3>
-                        </Button> 
-                    )
-                }
-                </Grid>
-
-            </Grid>
+                    <Tab label="Home" value="/"/>
+                    <Tab label="Requests" value="/requests"/>
+                    <Tab label="Group Chats" value="/group-chat"/>
+                    <Tab label="About Us" value="/about" disabled={true}/>
+                    { 
+                        this.props.userObject !== null ? (
+                            <Tab label="Profile" value="/profile"/>
+                        ) : (
+                            <Tab label="Login" value="/login"/>
+                        )
+                    }
+                </Tabs>
+            </AppBar>
         );
     }
 }
